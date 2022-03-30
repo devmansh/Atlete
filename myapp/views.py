39fecd2d5f03c68f14993f36447701af3933bcee
manojsharma1 <93111552+manojsharma1@users.noreply.github.com>
2022-03-30 13:47:45 +0530

    update 2

--- myapp/views.py
+++ myapp/views.py
@@ -278,18 +278,10 @@ def blog(request, pk):
 
 
 def news(request):
-    country = request.GET.get('country')
-    category = request.GET.get('category')
-    if country:
-        url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={API_KEY}'
-        response = requests.get(url)
-        data = response.json()
-        articles = data['articles']
-    else:
-        url = f'https://newsapi.org/v2/top-headlines?category={category}&apiKey={API_KEY}'
-        response = requests.get(url)
-        data = response.json()
-        articles = data['articles']
+    url = f'https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey={API_KEY}'
+    response = requests.get(url)
+    data = response.json()
+    articles = data['articles']
         
     context = {
         'articles' : articles   
